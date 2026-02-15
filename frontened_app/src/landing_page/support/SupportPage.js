6c8913e1ce3a6c53bbe293f26f14c54d6c8b98d7
ANKITA YADAV <145215095+aankiita@users.noreply.github.com>
2026-02-15 23:25:58 +0530

    Update SupportPage.js

--- frontened_app/src/landing_page/support/SupportPage.js
+++ frontened_app/src/landing_page/support/SupportPage.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
-
-import Navbar from "../Navbar";
-import Footer from "../Footer";
-
 function SupportPage() {
     return (  
         <>
